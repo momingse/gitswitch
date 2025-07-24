7fca564293a629de73a81f4193c3705ddb699657
momingse <da1710080850@gmail.com>
2025-07-24 22:29:39 +0800

    feat: add config

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"fs/cmd"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("No config file found; using defaults or env vars.")
+	}
+
 	cmd.Execute()
 }
